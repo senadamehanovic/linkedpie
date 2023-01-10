94a6118d41917ce4e2a056f82fe276a5c4060136
Senada Mehanovic <senadamehanovic@Senadas-MacBook-Pro.local>
2023-01-10 12:14:11 +0100

    test

---
+++ wp-content/plugins/social-link-pages/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '51e324600094c95dc3685be63b06dce6e2acf5ce',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '51e324600094c95dc3685be63b06dce6e2acf5ce',
+            'dev_requirement' => false,
+        ),
+    ),
+);
